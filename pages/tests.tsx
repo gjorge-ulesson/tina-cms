ad8b4e5fa9c18329ec14967976b2328cabf2702b
Gjorge Karakabakov <gorgekara@gmail.com>
2023-06-15 10:06:22 +0200

    [ADB-123] Tests page

---
+++ pages/tests.tsx
@@ -0,0 +1,14 @@
+import { Container } from "../components/util/container";
+import { Section } from "../components/util/section";
+import { Posts } from "../components/posts";
+import { Layout } from "../components/layout";
+
+export default function TestsPage() {
+  return (
+    <Layout>
+      <Section className="flex-1">
+        <h1>Tests</h1>
+      </Section>
+    </Layout>
+  );
+}
